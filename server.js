e9ada7d725d6fefd1b245ec0bc02c50ee33f9e6e
Kuldeep Raj <kuldeep_raj@jploft.in>
2026-01-24 15:57:23 +0530

    Changes as per new requirement

--- server.js
+++ server.js
@@ -33,6 +33,36 @@ app.get("/api/live-share", async (req, res) => {
   }
 });
 
+app.get("/api/live-share-clound", async (req, res) => {
+  console.log('clound service')
+  const uuid = req.query.uuid;
+  if (!uuid) {
+    return res.status(400).json({ error: "uuid is required" });
+  }
+
+  try {
+    const response = await fetch(`https://cloud.samsara.com/o/29533/fleet/viewer/tdmbHrRrIBcD9AOQcYrg`,
+      {
+        headers: {
+          "Accept": "application/json, text/plain, */*",
+          "Origin": "https://cloud.samsara.com",
+          "Referer": "https://cloud.samsara.com",
+          "User-Agent": "Mozilla/5.0",
+          "X-Web-Share-Api-Key": "3gCAa2VxLV3nlJfk7EhzJUEe5lg3IU9b50sNyOfUSSE6Fg2ACZr6GK5KqpMW55rn"
+        }
+      }
+    );
+    console.log('clound response');
+    console.log(response);
+
+    const data = await response.json();
+    res.send(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
